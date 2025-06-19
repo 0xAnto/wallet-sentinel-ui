a218124c030495cd01b340ae94fcb76592ab4b2f
0xAnto <antonyranjith17@gmail.com>
2025-06-19 14:17:52 +0530

    refactor: remove unused imports from aptos.ts

--- lib/aptos.ts
+++ lib/aptos.ts
@@ -1,3 +1,3 @@
-import { Account, AccountAddress, Aptos, AptosConfig, Ed25519PrivateKey, Network, NetworkToNetworkName, PrivateKey } from "@aptos-labs/ts-sdk";
+import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 const config = new AptosConfig({ network: Network.MAINNET });
 export const aptosClient = new Aptos(config);
